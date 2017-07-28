0b0f1c35e5eef1bc6a5092fac2891782dc5c402c
Alan Wei <alan.wei@shopback.com>
2017-07-28 18:35:11 +0800

    tweak

--- website/components/Button.js
+++ website/components/Button.js
@@ -1,59 +1,55 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Button } from '../../components/index.web';
 
-class Demo extends Component {
-  render() {
-    return (
-      <div className="component-content">
-        <div className="component-section">
-          <div className="component-title">Type</div>
-          <div className="component-subtitle">primary</div>
-          <Button
-            className="component-item"
-          >
-            <span>Primary</span>
-          </Button>
-          <div className="component-subtitle">ghost</div>
-          <Button
-            className="component-item"
-            type="ghost"
-          >
-            <span>Ghost</span>
-          </Button>
-          <div className="component-subtitle">blank</div>
-          <Button
-            className="component-item"
-            type="blank"
-          >
-            <span>Blank</span>
-          </Button>
-        </div>
-        <div className="component-section">
-          <div className="component-title">Size</div>
-          <div className="component-subtitle">large</div>
-          <Button
-            className="component-item"
-            size="large"
-          >
-            <span>Primary</span>
-          </Button>
-          <div className="component-subtitle">regular</div>
-          <Button
-            className="component-item"
-          >
-            <span>Primary</span>
-          </Button>
-          <div className="component-subtitle">small</div>
-          <Button
-            className="component-item"
-            size="small"
-          >
-            <span>Primary</span>
-          </Button>
-        </div>
-      </div>
-    );
-  }
-}
+const Demo = () => (
+  <div className="component-content">
+    <div className="component-section">
+      <div className="component-title">Type</div>
+      <div className="component-subtitle">primary</div>
+      <Button
+        className="component-item"
+      >
+        <span>Primary</span>
+      </Button>
+      <div className="component-subtitle">ghost</div>
+      <Button
+        className="component-item"
+        type="ghost"
+      >
+        <span>Ghost</span>
+      </Button>
+      <div className="component-subtitle">blank</div>
+      <Button
+        className="component-item"
+        type="blank"
+      >
+        <span>Blank</span>
+      </Button>
+    </div>
+    <div className="component-section">
+      <div className="component-title">Size</div>
+      <div className="component-subtitle">large</div>
+      <Button
+        className="component-item"
+        size="large"
+      >
+        <span>Primary</span>
+      </Button>
+      <div className="component-subtitle">regular</div>
+      <Button
+        className="component-item"
+      >
+        <span>Primary</span>
+      </Button>
+      <div className="component-subtitle">small</div>
+      <Button
+        className="component-item"
+        size="small"
+      >
+        <span>Primary</span>
+      </Button>
+    </div>
+  </div>
+);
 
 export default Demo;
--- website/components/Carousel.js
+++ website/components/Carousel.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import map from 'lodash/map';
 import { Carousel } from '../../components/index.web';
 
