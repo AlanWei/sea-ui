701caca6003249c05ec3ab270eb287a51d2a511e
Alan Wei <alan.wei@shopback.com>
2017-11-10 18:34:55 +0800

    add layout

--- src/views/components/index.js
+++ src/views/components/index.js
@@ -5,10 +5,30 @@ import Footer from '../container/Footer';
 import './style.scss';
 
 class Components extends Component {
+  renderSideBar = () => {
+    return (
+      <div className="sidebar">
+      </div>
+    );
+  }
+
+  renderMainContent = () => {
+    return (
+      <div className="main">
+      </div>
+    );
+  }
+
   render() {
     return (
       <div className="view-components">
         <Header />
+        <div className="content">
+          <div className="container">
+            {this.renderSideBar()}
+            {this.renderMainContent()}
+          </div>
+        </div>
         <Footer />
       </div>
     );
