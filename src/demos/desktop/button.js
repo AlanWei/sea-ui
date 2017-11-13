408759858548e0a18d49ff5e022dee881ca75f05
Alan Wei <alan.wei@shopback.com>
2017-11-13 18:24:45 +0800

    tweak desktop button demo

--- src/demos/desktop/button.js
+++ src/demos/desktop/button.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import { Button } from '../../../components';
 
+import './button.scss';
+
 class Demo extends Component {
   renderColorSection = () => (
     <div className="component-section">
       <div className="component-title">Color</div>
       <div className="component-subtitle">red (default)</div>
-      <Button className="component-item">Button</Button>
+      <Button className="component-item button-maxWidth">Button</Button>
       <div className="component-subtitle">green</div>
-      <Button className="component-item" uiColor="green">Button</Button>
+      <Button className="component-item button-maxWidth" uiColor="green">Button</Button>
       <div className="component-subtitle">blue</div>
-      <Button className="component-item" uiColor="blue">Button</Button>
+      <Button className="component-item button-maxWidth" uiColor="blue">Button</Button>
     </div>
   )
 
@@ -18,11 +20,11 @@ class Demo extends Component {
     <div className="component-section">
       <div className="component-title">Size</div>
       <div className="component-subtitle">md (default)</div>
-      <Button className="component-item">Button</Button>
+      <Button className="component-item button-maxWidth">Button</Button>
       <div className="component-subtitle">lg</div>
-      <Button className="component-item" uiSize="lg">Button</Button>
+      <Button className="component-item button-maxWidth" uiSize="lg">Button</Button>
       <div className="component-subtitle">s</div>
-      <Button className="component-item" uiSize="s">Button</Button>
+      <Button className="component-item button-maxWidth" uiSize="s">Button</Button>
     </div>
   )
 
@@ -30,11 +32,11 @@ class Demo extends Component {
     <div className="component-section">
       <div className="component-title">Type</div>
       <div className="component-subtitle">fill (default)</div>
-      <Button className="component-item">Button</Button>
+      <Button className="component-item button-maxWidth">Button</Button>
       <div className="component-subtitle">ghost</div>
-      <Button className="component-item" uiType="ghost">Button</Button>
+      <Button className="component-item button-maxWidth" uiType="ghost">Button</Button>
       <div className="component-subtitle">flat</div>
-      <Button className="component-item" uiType="flat">Button</Button>
+      <Button className="component-item button-maxWidth" uiType="flat">Button</Button>
     </div>
   )
 
